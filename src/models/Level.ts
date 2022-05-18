80fb8e76be7e2cbae83d62551a883564dca9c9f4
Nahuel Pierotti <npie@unlam.edu.ar>
2022-05-18 19:36:12 -0300

    Agrega bkp de datos. Agrega relacion de nivel y deporte

--- src/models/Level.ts
+++ src/models/Level.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Player } from "./Player";
+import { Sport } from "./Sport";
 
 @Entity()
 
@@ -13,5 +14,8 @@ export class Level extends BaseEntity{
 
     @OneToMany(() => Player, (player) => player.level)
     player: Player[]
+
+    @ManyToOne(() => Sport, (sport) => sport.level)
+    sport: Sport
 
 }
--- src/models/Sport.ts
+++ src/models/Sport.ts
@@ -1,4 +1,5 @@
 import { BaseEntity, Column, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Level } from "./Level";
 import { Player } from "./Player";
 import { Position } from "./Position";
 import { PreEvent } from "./PreEvent";
@@ -13,7 +14,9 @@ export class Sport extends BaseEntity{
     @Column()
     description: string
   
-    @Column()
+    @Column({
+        nullable: true,
+    })
     capacity: number
 
     @OneToMany(() => Position, (position) => position.sport)
@@ -24,4 +27,7 @@ export class Sport extends BaseEntity{
 
     @OneToOne(()=>PreEvent)
     preEvent: PreEvent;
+
+    @OneToMany(() => Level, (level) => level.sport)
+    level: Level[]
 }
